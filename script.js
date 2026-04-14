26c74b61edb924cb13c3fc892fff84048c79d22f
rssermonia <rssermonia@up.edu.ph>
2026-04-14 18:43:17 +0800

    added make card and load cards on window open

---
+++ script.js
@@ -0,0 +1,65 @@
+const container = document.getElementById("pokemonGrid");
+const template = document.getElementById("pokemonCardTemplate");
+
+function clearCont() {
+    container.innerHTML = "";
+}
+
+async function getPokemon(pokemon) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (!res.ok) throw new Error("Not found");
+    return res.json();
+}
+
+function makeCard(pokemon){
+    const clone = template.content.cloneNode(true);
+
+    const article = clone.querySelector(".pokemon");
+    const img = clone.querySelector("img");
+    const name = clone.querySelector(".pokemonName");
+    const typeContainer = clone.querySelector(".pokemonType");
+    const abilitiesContainer = clone.querySelector(".pokemonAbilities");
+
+    article.dataset.pokemonId = pokemon.id;
+    img.src = pokemon.sprites.front_default;
+    name.textContent = `${pokemon.name.toUpperCase()}`;
+    
+    pokemon.types.forEach(t => {
+        const span = document.createElement("span");
+        span.textContent = t.type.name;
+        span.classList.add("type");
+        typeContainer.appendChild(span);
+    });
+
+    pokemon.abilities.forEach(a => {
+        const span = document.createElement("span");
+        span.textContent = a.ability.name;
+        span.classList.add("ability");
+        abilitiesContainer.appendChild(span);
+    });
+
+    container.appendChild(clone);
+}
+
+async function loadPokemon() {
+    clearCont();
+
+    try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const data = await res.json();
+
+        const pokemonList = data.results;
+        const promises = pokemonList.map(p => getPokemon(p.name));
+
+        const gen1 = await Promise.all(promises);
+
+        gen1.forEach(pokemon => {
+            makeCard(pokemon);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", loadPokemon);
